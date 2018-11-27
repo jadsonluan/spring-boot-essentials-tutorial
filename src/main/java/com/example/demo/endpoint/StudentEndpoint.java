81aefc5e69ebacba63844e996d2a0f964e929c3d
Luan <jadsonluan1@gmail.com>
2018-11-27 14:42:56 -0300

    Spring Boot Essentials 10

--- src/main/java/com/example/demo/endpoint/StudentEndpoint.java
+++ src/main/java/com/example/demo/endpoint/StudentEndpoint.java
@@ -1,12 +1,15 @@
 package com.example.demo.endpoint;
 
 import com.example.demo.error.CustomErrorType;
+import com.example.demo.error.ResourceNotFoundException;
 import com.example.demo.model.Student;
 import com.example.demo.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("students")
@@ -25,10 +28,15 @@ public class StudentEndpoint {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> findStudentById(@PathVariable("id") long id) {
+        verifyIfStudentExists(id);
         Student student = studentDAO.findById(id).get();
-        if (student == null)
-            return new ResponseEntity<>(new CustomErrorType(("Student not found")), HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    private void verifyIfStudentExists(long id) {
+        Optional<Student> student = studentDAO.findById(id);
+        if (!student.isPresent())
+            throw new ResourceNotFoundException("Student not found for id " + id);
     }
 
     @GetMapping("/findByName/{name}")
@@ -43,12 +51,14 @@ public class StudentEndpoint {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") long id) {
+        verifyIfStudentExists(id);
         studentDAO.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping
     public ResponseEntity<?> update(@RequestBody Student student) {
+        verifyIfStudentExists(student.getId());
         studentDAO.save(student);
         return new ResponseEntity<>(HttpStatus.OK);
     }
